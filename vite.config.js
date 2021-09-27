5c8bbc2f95433297692f3245f40a8980b7a1c113
Vaishnav Sharma <30750862+vaishnav@users.noreply.github.com>
2021-09-27 23:44:58 +0530

    debug attempt 3

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/dist/',
+  base: '/threejs_scene/',
   build: {
     chunkSizeWarningLimit:1500,
     rollupOptions: {
